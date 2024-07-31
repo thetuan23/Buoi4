b7e15948d1521ee2d9624f222cb107cb5db97e19
thetuan23 <lethetuan2312@gmail.com>
2024-07-31 10:10:30 +0700

    Hoàn Thành BT

---
+++ ThuatToanThamLam/ThuatToanThamLam/Source.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct {
+	int start;
+	int finish;
+} Activity;
+
+
+int compare(const void *a, const void *b) {
+	Activity *activityA = (Activity *)a;
+	Activity *activityB = (Activity *)b;
+	return activityA->finish - activityB->finish;
+}
+
+
+void printMaxActivities(Activity activities[], int n) {
+
+	qsort(activities, n, sizeof(Activity), compare);
+
+	printf("Cac hoat dong duoc chon:\n");
+
+	int i = 0;
+	printf("(%d, %d)\n", activities[i].start, activities[i].finish);
+
+
+	for (int j = 1; j < n; j++) {
+	
+		if (activities[j].start >= activities[i].finish) {
+			printf("(%d, %d)\n", activities[j].start, activities[j].finish);
+			i = j;
+		}
+	}
+}
+
+int main() {
+	Activity activities[] = { { 1, 3 }, { 2, 4 }, { 0, 6 }, { 5, 7 }, { 8, 9 }, { 5, 9 } };
+	int n = sizeof(activities) / sizeof(activities[0]);
+
+	printMaxActivities(activities, n);
+	getchar();
+	return 0;
+}
